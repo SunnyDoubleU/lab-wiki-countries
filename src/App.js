26b614f783b21d5b2bff3522e0b595ba54e4d960
Sunny Wong <sunnywong18@gmail.com>
2019-10-17 16:22:18 +0200

    lab complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import countries from './data/countries.json'
+import CountryDetail from './components/CountryDetail'
+
+import { Link, Route } from "react-router-dom";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App" >
+      <h1 className="list-group-item list-group-item-action active">WikiCountries</h1>
+      <div className="AppRow">
+        <div className="list-group SideBar">
+          {countries.map((country) =>
+            <Link className="list-group-item list-group-item-action" to={`/country-detail/${country.cca3}`}>{country.flag.normalize()}{country.name.common}</Link>
+          )}
+        </div>
+        <div className="Pages">
+          <Route path="/country-detail/:cca3" component={CountryDetail} />
+        </div>
+      </div>
+    </div >
   );
 }
 
---
+++ src/components/CountryDetail.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import countries from '../data/countries.json'
+import { Link } from "react-router-dom";
+
+
+const CountryDetail = (props) => {
+
+    var countryDetail = props.match.params.cca3
+    var filteredCountry = countries.filter((country) => country.cca3 === countryDetail)[0]
+
+    return (
+        <div className="listGroup">
+            <h1>{filteredCountry.name.common}</h1>
+            <h3>Capital: {filteredCountry.capital}</h3>
+            <h3>Area: {filteredCountry.area} km<sup>2</sup></h3>
+            <h4>Borders:</h4>
+            <ul>
+                {filteredCountry.borders.map((border) => {
+                    let find = countries.filter((country) => country.cca3 === border)[0]
+                    return (
+                        < li > <Link to={`/country-detail/${border}`}>{find.name.common}</Link></li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
+}
+
+export default CountryDetail;
